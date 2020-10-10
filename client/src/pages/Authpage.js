3fbef39be0795d95ddbabc96052e7f3509a8e475
NatalGrython <71853222+NatalGrython@users.noreply.github.com>
2020-10-10 19:44:51 +0300

    Modified react files

--- client/src/pages/Authpage.js
+++ client/src/pages/Authpage.js
@@ -6,7 +6,7 @@ export const Authpage = () => {
             <div className = "col s6 offset-s3">
                 <h1>Пиздарики</h1>
                 <div className = "card blue darcken-1">
-                    
+                   <div 
                 </div>
             </div>
             
